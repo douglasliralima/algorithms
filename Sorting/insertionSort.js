786abbbabe0bf130cefcf6c225bcfc49e305b4a2
douglasliralima <douglasliralima@gmail.com>
2023-01-22 13:27:22 -0300

    insertion sort self made

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,28 @@
+function swap(arr, i, j) {
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+/**
+ * 
+ * @param {Array<number>} arr 
+ */
+function insertionSort(arr) {
+    /**
+     * I need to see, for the unordered partion, we should
+     * - Compare with the predecessors, until we find the place to insert it
+     * - when we find the place to insert it, we should push everybody to right
+     */
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j >= 1; j--) {
+            console.log(arr, i, j, j - 1)
+            if (arr[j] < arr[j - 1]) {
+                swap(arr, j, j - 1);
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(insertionSort([4001, 122, 55, 61]))
